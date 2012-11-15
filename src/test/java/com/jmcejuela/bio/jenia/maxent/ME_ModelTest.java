c53f58b667eefaaae6437522d98bf0f91f871896
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-15 16:20:43 +0100

    init ME_ModelTest

---
+++ src/test/java/com/jmcejuela/bio/jenia/maxent/ME_ModelTest.java
@@ -0,0 +1,14 @@
+package com.jmcejuela.bio.jenia.maxent;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ME_ModelTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}

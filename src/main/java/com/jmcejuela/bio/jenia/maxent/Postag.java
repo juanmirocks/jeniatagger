302308799212f37586b7221008167ab470072758
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-15 21:12:05 +0100

    added postag class (though unimplemented)

---
+++ src/main/java/com/jmcejuela/bio/jenia/maxent/Postag.java
@@ -0,0 +1,15 @@
+package com.jmcejuela.bio.jenia.maxent;
+
+import javax.naming.OperationNotSupportedException;
+
+/**
+ * From postag.cpp
+ *
+ * Not implemented. It seems to be deprecated in original.
+ * 
+ */
+public class Postag {
+  public Postag() throws OperationNotSupportedException {
+    throw new OperationNotSupportedException("Not implemented yet");
+  }
+}

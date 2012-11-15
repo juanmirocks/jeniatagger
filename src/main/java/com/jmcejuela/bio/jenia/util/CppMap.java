6bd004592323a8457e6a68f73c32af806fcb5f50
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-15 20:25:33 +0100

    map with same behavior as std:map with operator[]

---
+++ src/main/java/com/jmcejuela/bio/jenia/util/CppMap.java
@@ -0,0 +1,32 @@
+package com.jmcejuela.bio.jenia.util;
+
+import java.util.HashMap;
+
+/**
+ * std::map::operator[] (http://www.cplusplus.com/reference/stl/map/operator[]/)
+ *
+ * if key is not in the map, the key is inserted with a new value initialized with the default constructor.
+ *
+ * Due to type-erasure in Java a object generator must be given explicitly to simulate the same behavior.
+ */
+public class CppMap<K, V> extends HashMap<K, V> {
+  Creator<V> c;
+
+  private static final long serialVersionUID = -197420711650949268L;
+
+  public CppMap(Creator<V> c) {
+    this.c = c;
+  }
+
+  // can't verify the type due to type erasure -> (K) cast
+  @SuppressWarnings("unchecked")
+  @Override
+  public V get(Object key) {
+    V ret = super.get(key);
+    if (null == ret) {
+      ret = c.neu();
+      super.put((K) key, ret);
+    }
+    return ret;
+  }
+}

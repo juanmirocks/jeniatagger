74d69c0134646a33346f9e5c46404ee1c0aafc02
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-16 10:58:36 +0100

    fixed ME_ModelTest

--- src/test/java/com/jmcejuela/bio/jenia/maxent/ME_ModelTest.java
+++ src/test/java/com/jmcejuela/bio/jenia/maxent/ME_ModelTest.java
@@ -8,27 +8,38 @@ public class ME_ModelTest {
 
   @Test
   public void test() {
-    // 16 files, numerated from 0 to 15
-    for (int i = 0; i < 16; i++) {
-      ME_Model me = new ME_Model();
-      me.load_from_file("/models_medline/model.bidir." + i);
+    testModel("/models_medline/model.bidir.0", 105262);
+    testModel("/models_medline/model.bidir.1", 98818);
+    testModel("/models_medline/model.bidir.2", 103152);
+    testModel("/models_medline/model.bidir.3", 102679);
+    testModel("/models_medline/model.bidir.4", 101320);
+    testModel("/models_medline/model.bidir.5", 100651);
+    testModel("/models_medline/model.bidir.6", 107283);
+    testModel("/models_medline/model.bidir.7", 111653);
+    testModel("/models_medline/model.bidir.8", 101580);
+    testModel("/models_medline/model.bidir.9", 98305);
+    testModel("/models_medline/model.bidir.10", 98513);
+    testModel("/models_medline/model.bidir.11", 102116);
+    testModel("/models_medline/model.bidir.12", 102449);
+    testModel("/models_medline/model.bidir.13", 99862);
+    testModel("/models_medline/model.bidir.14", 108613);
+    testModel("/models_medline/model.bidir.15", 114054);
 
-      testModel(me, 105262);
-    }
+    testModel("/models_chunking/model.bidir.0", 159110);
+    testModel("/models_chunking/model.bidir.2", 96736);
+    testModel("/models_chunking/model.bidir.4", 95383);
+    testModel("/models_chunking/model.bidir.6", 36225);
+  }
+
+  public static <T> void testModel(String meResource, int numLinesFile) {
+    ME_Model me = new ME_Model();
+    me.load_from_file(meResource);
+    testModel(me, numLinesFile);
   }
 
   public static <T> void testModel(ME_Model me, int numLinesFile) {
-    // assertEquals(numLinesFile, me._label_bag._size);
-    // assertEquals(numLinesFile, me._label_bag.Size());
-
-    // assertEquals(numLinesFile, me._featurename_bag._size);
-    // assertEquals(numLinesFile, me._featurename_bag.Size());
-
-    // assertEquals(numLinesFile, me._fb.Size());
-
-    // assertEquals(numLinesFile, me._vl.size());
-
-    // assertEquals(numLinesFile, me._num_classes);
+    assertEquals(numLinesFile, me._fb.Size());
+    assertEquals(numLinesFile, me._vl.size());
   }
 
 }

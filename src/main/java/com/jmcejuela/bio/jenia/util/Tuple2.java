38688713e6ecbe7bea9bb109f87b016f74b6c908
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-16 18:39:40 +0100

    toString for tuple

--- src/main/java/com/jmcejuela/bio/jenia/util/Tuple2.java
+++ src/main/java/com/jmcejuela/bio/jenia/util/Tuple2.java
@@ -2,7 +2,7 @@ package com.jmcejuela.bio.jenia.util;
 
 /**
  * C++ std::pair equivalent as a Scala-like tuple.
- * 
+ *
  * @param <T1>
  * @param <T2>
  */
@@ -17,6 +17,11 @@ public class Tuple2<T1, T2> {
 
   public static <T1, T2> Tuple2<T1, T2> $(T1 _1, T2 _2) {
     return new Tuple2<T1, T2>(_1, _2);
+  }
+
+  @Override
+  public String toString() {
+    return "(" + _1 + ", " + _2 + ")";
   }
 
 }

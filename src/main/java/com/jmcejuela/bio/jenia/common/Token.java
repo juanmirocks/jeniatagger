e6c310670694b8aacd4c99b4bc0953c0aadd03c7
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-16 09:56:32 +0100

    made final fields as possible and default fields to the empty string to simulate c++ behavior

--- src/main/java/com/jmcejuela/bio/jenia/common/Token.java
+++ src/main/java/com/jmcejuela/bio/jenia/common/Token.java
@@ -4,15 +4,20 @@ package com.jmcejuela.bio.jenia.common;
  * From common.h
  */
 public class Token {
-  public String str;
+  public final String str;
   public String pos;
   public String prd;
   public String cprd; // for chunking
-  public String tag; // for chunking
+  public final String tag; // for chunking
   public String ne;
 
   public Token(String s, String p) {
     str = s;
     pos = p;
+    // Must set to empty String to simulate c++ string default constructor behavior
+    prd = "";
+    cprd = "";
+    tag = "";
+    ne = "";
   }
 }

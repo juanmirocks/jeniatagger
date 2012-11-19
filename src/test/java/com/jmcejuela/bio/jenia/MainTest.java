fd908825808935795f79f66641fc7e151310886a
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-19 11:35:41 +0100

    test with file

--- src/test/java/com/jmcejuela/bio/jenia/MainTest.java
+++ src/test/java/com/jmcejuela/bio/jenia/MainTest.java
@@ -1,15 +1,27 @@
 package com.jmcejuela.bio.jenia;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.junit.Test;
+
+import com.jmcejuela.bio.jenia.util.Util;
 
 public class MainTest {
 
   @Test
   public void testNothingCrashes() throws IOException, InterruptedException {
-    Main.main(new String[] { "-nt" }); // keeps waiting forever for input
-    // Thread.sleep(20 * 1000);
+    System.setIn(Util.resourceStream("/genia-nt.in"));
+    File tmpOut = File.createTempFile("test-genia-nt", ".out");
+    System.out.println("tmp out file: " + tmpOut.getAbsolutePath());
+    System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(tmpOut))));
+
+    Main.main(new String[] { "-nt" });
+
+    System.err.println("Done");
     return;
   }
 

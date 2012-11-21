14857b0d581d62af6fcef3f98d81ea31ed85cc59
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-21 11:42:36 +0100

    added test for bc2gm training data (known to fail) and activated test for unicode characters

--- src/test/java/com/jmcejuela/bio/jenia/MainTest.java
+++ src/test/java/com/jmcejuela/bio/jenia/MainTest.java
@@ -11,25 +11,51 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.jmcejuela.bio.jenia.util.Util;
 
 public class MainTest {
 
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+    //Force the dictionaries to be loaded only once at the beginning
+    JeniaTagger.analyzeAll("load", true);
+  }
+
   @Test
-  public void testSameAsOriginalOutputWithNT() throws IOException {
-    System.setIn(Util.resourceStream("/genia-nt.in"));
-    File tmpOut = File.createTempFile("test-genia-nt", ".out");
-    System.out.println("genia -nt test: tmp out file: " + tmpOut.getAbsolutePath());
+  public void testOutputWithNT() throws IOException {
+    testSameAsOriginalOutput("/genia-nt.in", "/genia-nt.out", "-nt");
+  }
+
+  /**
+   * TODO known to fail for named-entity recognition in some instances
+   *
+   * The test file is very big (the output is 8.8MB)
+   *
+   * It should throughly test whether jenia's output is the same as genia's. The Bc2Gm training data
+   * contained various entities for all named-classes recognized by the genia tagger.
+   *
+   * @throws IOException
+   */
+  @Test
+  public void testOutputWithNTBc2GmTrainingData() throws IOException {
+    testSameAsOriginalOutput("genia-nt.bc2gm-training.in", "genia-nt.bc2gm-training.out", "-nt");
+  }
+
+  public void testSameAsOriginalOutput(String in, String out, String arguments) throws IOException {
+    System.setIn(Util.resourceStream(in));
+    File tmpOut = File.createTempFile("test-" + in, ".out");
+    System.out.println(in + " test, tmp out file:" + tmpOut.getAbsolutePath());
     System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(tmpOut))));
 
     long before = System.currentTimeMillis();
-    Main.main(new String[] { "-nt" });
+    Main.main(arguments.trim().split("\\s+"));
     long time = System.currentTimeMillis() - before;
     System.err.println("Done (" + (time / 1000) + " s). Now comparing the output files, line by line");
 
-    BufferedReader expectOutput = new BufferedReader(new InputStreamReader(Util.resourceStream("/genia-nt.out")));
+    BufferedReader expectOutput = new BufferedReader(new InputStreamReader(Util.resourceStream(out)));
     BufferedReader actualOutput = new BufferedReader(new FileReader(tmpOut));
 
     String expectLine = null;
@@ -48,16 +74,20 @@ public class MainTest {
     actualOutput.close();
   }
 
+  /**
+   * TODO known to have different output for named-entity recognition with unicode characters.
+   *
+   * The original geniatagger actually does not handle unicode well.
+   *
+   * @throws IOException
+   */
   @Test
   public void testUnicodeDoesNotCrash() throws IOException {
-    // TODO Known to have different output with unicode characters. The original geniatagger
-    // actually does not handle them well
-
-    // System.setIn(Util.resourceStream("/genia-unicode.in"));
-    // File tmpOut = File.createTempFile("test-genia-unicode", ".out");
-    // System.out.println("genia unicode test: tmp out file: " + tmpOut.getAbsolutePath());
-    // System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(tmpOut))));
-    //
-    // Main.main(new String[] { "-nt" });
+    System.setIn(Util.resourceStream("/genia-unicode.in"));
+    File tmpOut = File.createTempFile("test-genia-unicode", ".out");
+    System.out.println("genia unicode test: tmp out file: " + tmpOut.getAbsolutePath());
+    System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(tmpOut))));
+    Main.main(new String[] { "-nt" });
   }
+
 }

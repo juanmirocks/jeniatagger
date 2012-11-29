1c088699f5664dbabb4269a7fb6345fd2b6a6335
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-29 11:43:34 +0100

    bug fix in test. Couldn't create temp file because of leading /

--- src/test/java/com/jmcejuela/bio/jenia/MainTest.java
+++ src/test/java/com/jmcejuela/bio/jenia/MainTest.java
@@ -26,7 +26,7 @@ public class MainTest {
 
   @Test
   public void testOutputNT() throws IOException {
-    testSameAsOriginalOutput("/genia-nt.in", "/genia-nt.out", "-nt");
+    testSameAsOriginalOutput("genia-nt.in", "genia-nt.out", "-nt");
   }
 
   /**

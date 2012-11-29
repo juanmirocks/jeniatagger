15f5c2171f61247d2b1027f4216080af05311b86
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2012-11-29 16:24:37 +0100

    removed old original commented-out code

--- src/main/java/com/jmcejuela/bio/jenia/maxent/ME_Model.java
+++ src/main/java/com/jmcejuela/bio/jenia/maxent/ME_Model.java
@@ -434,8 +434,6 @@ public class ME_Model {
     for (int label = 0; label < _num_classes; label++) {
       double pow = powv[label] - offset;
       double prod = exp(pow);
-      // cout << pow << " " << prod << ", ";
-      // if (_ref_modelp != NULL) prod *= _train_refpd[n][label];
       if (_ref_modelp != null) prod *= s.ref_pd.get(label);
       assert (prod != 0);
       membp[label] = prod;
